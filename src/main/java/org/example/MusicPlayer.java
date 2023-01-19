5d3d9929c221ef0b9035ae953569b5c60deb20a9
ElenaShylionak <99590421+ElenaShylionak@users.noreply.github.com>
2023-01-19 15:56:22 +0300

    DE with setter

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -5,12 +5,17 @@ public class MusicPlayer {
     private Music music;  //Common interface
 
     //IoC
-    public MusicPlayer(Music music) {
+    public MusicPlayer(Music music) {  //with arguments
+        this.music = music;
+    }
+    public MusicPlayer(){ //no arguments
+    }
+    public void setMusic(Music music) {
         this.music = music;
     }
 
     public void playMusic() {
-        System.out.println("Playing" + music.getSong());
+        System.out.println("Playing: " + music.getSong());
 
     }
 }
