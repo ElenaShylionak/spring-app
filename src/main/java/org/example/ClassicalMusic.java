9b3ed78997c7843fdfb84a43962cc879bf9da0a9
ElenaShylionak <99590421+ElenaShylionak@users.noreply.github.com>
2023-01-23 21:31:57 +0300

    DE - add factory-method="getClassicalMusic"

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,6 +1,15 @@
 package org.example;
 
 public class ClassicalMusic implements Music{
+
+    private ClassicalMusic() { //create a private constructor. So we are limited to creating objects of this class (new)
+    }
+
+    public static ClassicalMusic getClassicalMusic() { //create a factory method
+        return new ClassicalMusic();
+
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
